cfec089d319cd1ba4b213c4ac76ca1b35972a72a
Paing Soe Thu <whoistheblack@gmail.com>
2025-02-03 02:08:15 +0700

    feat: add some codes

---
+++ MiniBankingProject.Domain/Features/Transaction/TransactionService.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniBankingProject.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBankingProject.Domain.Features.Transaction
+{
+    public class TransactionService
+    {
+        private readonly AppDbContext _db = new AppDbContext();
+
+        public List<TblTransaction> GetTransactions()
+        {
+            var model = _db.TblTransactions.AsNoTracking().ToList();
+
+            return model;
+        }
+
+        public TblTransaction GetTransaction(int id)
+        {
+            var item = _db.TblTransactions
+                .AsNoTracking()
+                .FirstOrDefault(x => x.TransactionId == id);
+            return item;
+        }
+
+        public TblTransaction CreateTransaction(TblTransaction transaction)
+        {
+            _db.TblTransactions.Add(transaction);
+            _db.SaveChanges();
+            return transaction;
+        }
+
+        public TblTransaction UpdateTransaction(int id, TblTransaction transaction)
+        {
+            var item = _db.TblTransactions
+                .AsNoTracking()
+                .FirstOrDefault(x => x.TransactionId == id);
+            if (item is null)
+            {
+                return null;
+            }
+
+            item.FromMobileNo = transaction.FromMobileNo;
+            item.ToMobileNo = transaction.ToMobileNo;
+            item.TransferedAmount = transaction.TransferedAmount;
+            item.Dates = DateTime.Now;
+            item.Notes = transaction.Notes;
+
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return transaction;
+        }
+
+        public TblTransaction PatchTransaction(int id, TblTransaction transaction)
+        {
+            var item = _db.TblTransactions
+                .AsNoTracking()
+                .FirstOrDefault(x => x.TransactionId == id);
+
+            if (item is null)
+            {
+                return null;
+            }
+            if (!string.IsNullOrEmpty(transaction.FromMobileNo))
+            {
+                item.FromMobileNo = transaction.FromMobileNo;
+            }
+            if (!string.IsNullOrEmpty(transaction.ToMobileNo))
+            {
+                item.ToMobileNo = transaction.ToMobileNo;
+            }
+            if (transaction.TransferedAmount > 0)
+            {
+                item.TransferedAmount = transaction.TransferedAmount;
+            }
+            if (!string.IsNullOrEmpty(transaction.Notes))
+            {
+                item.Notes = transaction.Notes;
+            }
+
+            item.Dates = DateTime.Now;
+
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return transaction;
+        }
+
+        public bool? DeleteTransaction(int id)
+        {
+            var item = _db.TblTransactions
+                .AsNoTracking()
+                .FirstOrDefault(x => x.TransactionId == id);
+            if (item is null)
+            {
+                return null;
+            }
+            _db.Entry(item).State = EntityState.Deleted;
+            var result = _db.SaveChanges();
+
+            return result > 0;
+        }
+    }
+}
---
+++ MiniBankingProject.Domain/Features/User/UserService.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using MiniBankingProject.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBankingProject.Domain.Features.User
+{
+    public class UserService
+    {
+        private readonly AppDbContext _db = new AppDbContext();
+
+        public List<TblUser> GetUsers()
+        {
+            var model = _db.TblUsers.AsNoTracking().ToList();
+
+            return model;
+        }
+
+        public TblUser GetUser(int id)
+        {
+            var item = _db.TblUsers
+                .AsNoTracking()
+                .FirstOrDefault(x => x.UserId == id);
+            return item;
+        }
+
+        public TblUser CreateUser(TblUser user)
+        {
+            _db.TblUsers.Add(user);
+            _db.SaveChanges();
+            return user;
+        }
+
+        public TblUser UpdateUser(int id, TblUser user)
+        {
+            var item = _db.TblUsers
+                .AsNoTracking()
+                .FirstOrDefault(x => x.UserId == id);
+            if (item is null)
+            {
+                return null;
+            }
+            
+            item.FullName = user.FullName;
+            item.MobileNo = user.MobileNo;
+            item.Balance = user.Balance;
+            item.Pin = user.Pin;
+            item.UpdatedDate = DateTime.Now;
+
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return user;
+        }
+
+        public TblUser PatchUser(int id, TblUser user)
+        {
+            var item = _db.TblUsers
+                .AsNoTracking()
+                .FirstOrDefault(x => x.UserId == id);
+            if (item is null)
+            {
+                return null;
+            }
+            if (!string.IsNullOrEmpty(user.FullName))
+            {
+                item.FullName = user.FullName;
+            }
+            if (!string.IsNullOrEmpty(user.MobileNo))
+            {
+                item.MobileNo = user.MobileNo;
+            }
+            if (user.Balance > 0)
+            {
+                item.Balance = user.Balance;
+            }
+            if (!string.IsNullOrEmpty(user.Pin))
+            {
+                item.Pin = user.Pin;
+            }
+            
+            item.UpdatedDate = DateTime.Now;
+            
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return user;
+        }
+
+        public bool? DeleteUser(int id)
+        {
+            var item = _db.TblUsers
+                .AsNoTracking()
+                .FirstOrDefault(x => x.UserId == id);
+            if (item is null)
+            {
+                return null;
+            }
+            _db.Entry(item).State = EntityState.Deleted;
+            var result = _db.SaveChanges();
+
+            return result > 0;
+        }
+
+    }
+}
